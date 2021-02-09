c3ade6435da71b59199b859d08ca155864eebd3f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-09 21:01:44 +0200

    Add files via upload

---
+++ Dance Hall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance_Hall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomLenght = double.Parse(Console.ReadLine());
+            double roomWidth = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+
+            double roomArea = (roomLenght * 100) * (roomWidth * 100);
+
+            double wardrobeArea = (wardrobeSide * 100) * (wardrobeSide * 100);
+
+            double benchArea = roomArea / 10;
+
+            double freeSpace = roomArea - wardrobeArea - benchArea;
+
+            double dancersCount = freeSpace / 7040;
+
+            Console.WriteLine(Math.Floor(dancersCount));
+        }
+    }
+}
