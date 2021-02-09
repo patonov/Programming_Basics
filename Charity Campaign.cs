6aaaf9fd61ac97d445a2ba9cb7e45bbdc9f1f8de
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-09 21:00:55 +0200

    Add files via upload

---
+++ Charity Campaign.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charity_Campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int confectioners = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int gof = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakePrice = cakes * 45;
+            double gofPrice = gof * 5.80;
+            double pancakePrice = pancakes * 3.20;
+
+            double dayAmount = (cakePrice + gofPrice + pancakePrice) * confectioners;
+            double campainAmount = dayAmount * days;
+
+            double amountDisposable = campainAmount - (campainAmount / 8);
+
+            Console.WriteLine("{0:F2}", amountDisposable);
+        }
+    }
+}
