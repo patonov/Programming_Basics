42db8d332552a053c42c9ea2bd07e1301b4e7739
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-21 22:02:19 +0200

    Add files via upload

---
+++ FishTank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = length * width * height;
+            double liters = volume * 0.001;
+            double perCents = percent * 0.01;
+            double litersNeeded = liters * (1 - perCents);
+
+            Console.WriteLine("{0:F3}", litersNeeded);
+        }
+    }
+}
