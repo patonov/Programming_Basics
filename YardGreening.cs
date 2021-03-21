1e7c1822f8f7b6b18a267fa74520f2689e565047
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-21 21:56:54 +0200

    Add files via upload

---
+++ YardGreening.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int area = int.Parse(Console.ReadLine());
+            double price = area * 7.61;
+            double discount = 0.18 * price;
+            double finalPrice = price - discount;
+
+            Console.WriteLine("The final price is: {0:F2} lv.", finalPrice);
+            Console.WriteLine("The discount is: {0:F2} lv.", discount);
+        }
+    }
+}
