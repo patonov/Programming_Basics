46b593ada6d97c56867a39822ce01ba6fa901a8d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-26 20:17:21 +0200

    Add files via upload

---
+++ Graduation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 1;
+            double sum = 0;
+
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4.00)
+                {
+                    sum += grade;
+                    counter++;
+                }
+            }
+            double average = sum / 12;
+
+            Console.WriteLine("{0} graduated. Average grade: {1:F2}", name, average);
+        }
+    }
+}
