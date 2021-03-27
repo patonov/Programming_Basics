e61621e1809f5f74204c30ba69ac3dccfdf92716
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-03-27 21:57:27 +0200

    Add files via upload

---
+++ GreaterNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFirst = int.Parse(Console.ReadLine());
+            int numberSecond = int.Parse(Console.ReadLine());
+
+            if (numberFirst > numberSecond)
+            {
+                Console.WriteLine(numberFirst);
+            }
+            else
+            {
+                Console.WriteLine(numberSecond);
+            }
+        }
+    }
+}
