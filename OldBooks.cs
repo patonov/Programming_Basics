2037fccad449b966b8c3c0764088ed77bee4f2f8
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-08 21:50:04 +0300

    Add files via upload

---
+++ OldBooks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int libraryCapacity = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+            string searchBook = string.Empty;
+
+            while (counter <= libraryCapacity)
+            {
+                searchBook = Console.ReadLine();
+
+                if (book == searchBook)
+                {
+                    Console.WriteLine("You checked {0} books and found it.", counter - 1);
+                    break;
+                }
+
+                if (counter == libraryCapacity)
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine("You checked {0} books.", libraryCapacity);
+                }
+
+                counter++;
+            }
+        }
+    }
+}
