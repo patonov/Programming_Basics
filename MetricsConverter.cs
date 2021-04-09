344b65f82c877a251e50023039fd268a5369fd9f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-09 22:13:43 +0300

    Add files via upload

---
+++ MetricsConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MetricsConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+
+            if (input == "cm")
+            {
+                number /= 100;
+            }
+
+            else if (input == "mm")
+            {
+                number /= 1000;
+            }
+
+            if (output == "cm")
+            {
+                number *= 100;
+            }
+
+            else if (output == "mm")
+            {
+                number *= 1000;
+            }
+
+            Console.WriteLine("{0:F3}", number);
+
+        }
+    }
+}
