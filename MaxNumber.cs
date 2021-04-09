22a5fd852344d3af136cd80b019f5192f9e07252
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-09 22:16:02 +0300

    Add files via upload

---
+++ MaxNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            int max = int.MinValue;
+
+            while (count < n)
+            {
+                int num = int.Parse(Console.ReadLine());
+                count++;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
