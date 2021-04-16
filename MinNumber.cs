c906a47ad1b9a9b4c0be37aafec595001d370d25
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-16 21:43:50 +0300

    Add files via upload

---
+++ MinNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            int min = int.MaxValue;
+
+            while (count < n)
+            {
+                int num = int.Parse(Console.ReadLine());
+                count++;
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
