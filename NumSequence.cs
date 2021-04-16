525b95cf0b961f753f6fc41b658411528f223a2c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-16 21:44:13 +0300

    Add files via upload

---
+++ NumSequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biggest = int.MinValue;
+            int smallest = int.MaxValue;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+                if (num > biggest)
+                {
+                    biggest = num;
+                }
+            }
+            Console.WriteLine("Max number: {0}", biggest);
+            Console.WriteLine("Min number: {0}", smallest);
+        }
+    }
+}
