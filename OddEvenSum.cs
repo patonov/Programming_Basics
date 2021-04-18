a679ca99f17b4420a9769c5471eb5d81a5dacc3d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-18 22:41:26 +0300

    Add files via upload

---
+++ OddEvenSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int odd = 0;
+            int even = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    even += num;
+                }
+                else
+                {
+                    odd += num;
+                }
+
+            }
+
+            int diff = Math.Abs(odd - even);
+
+            if (diff == 0)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + odd);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
