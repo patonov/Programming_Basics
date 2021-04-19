1b6eb047807adbffe41094789fb723658cd5f9cf
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-19 20:25:11 +0300

    Add files via upload

---
+++ PasswordGuess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = Console.ReadLine();
+
+            string passWord = "s3cr3t!P@ssw0rd";
+
+            if (pass == passWord)
+            {
+                Console.WriteLine("Welcome");
+            }
+
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
