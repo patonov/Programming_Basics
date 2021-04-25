5a37de51264a3313dd93b9c22398f3322fb6168f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-25 21:30:13 +0300

    Add files via upload

---
+++ ProjectsCreation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architect = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            int hours = number * 3;
+
+            Console.WriteLine("The architect {0} will need {1} hours to complete {2} project/s.", architect, hours, number);
+        }
+    }
+}
