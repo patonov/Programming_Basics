bf04f0656ae24f18d731a2d36543dcae786b6a3a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-04-29 22:15:40 +0300

    Add files via upload

---
+++ ReadNums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReadNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            while (number < 1 || number > 100)
+            {
+                Console.WriteLine("Invalid number!");
+
+                number = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("The number is: {0}", number);
+        }
+    }
+}
