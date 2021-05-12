77613ca13595fbc8e226d804fa0d4c6821024058
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-05-12 18:14:13 +0300

    Add files via upload

---
+++ SumSeconds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeFirst = int.Parse(Console.ReadLine());
+            int timeSecond = int.Parse(Console.ReadLine());
+            int timeThird = int.Parse(Console.ReadLine());
+
+            int totalTime = timeFirst + timeSecond + timeThird;
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine("{0}:0{1}", minutes, seconds);
+            }
+            else
+            {
+                Console.WriteLine("{0}:{1}", minutes, seconds);
+            }
+
+        }
+    }
+}
