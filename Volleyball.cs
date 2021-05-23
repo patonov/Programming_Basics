cd365ad1d431d7e5b19fc196680e30857b1b50a6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-05-23 22:45:42 +0300

    Add files via upload

---
+++ Volleyball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double p = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double result = h + (48 - h) * 3 / 4 + p * 2 / 3;
+
+            if (input == "leap")
+            {
+                result = (double)(result + (result * 15 / 100));
+            }
+            Console.WriteLine((int)result);
+        }
+    }
+    
+}
